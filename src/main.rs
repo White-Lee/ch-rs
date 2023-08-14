ab4c7bb99ff3c7d8829a14389a704466bd13dcf6
White Lee <whitelee8080@gmail.com>
2023-08-14 23:35:16 +0800

    feat：init log4rs config

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 #![windows_subsystem = "windows"]
 
 use log4rs;
-use log::{error, info, warn};
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     init_logging();
-    info!("开始");
-    warn!("booting up");
     let ui = AppWindow::new()?;
     ui.run()
 }
 
 fn init_logging() {
     log4rs::init_file("conf/log4rs.yaml", Default::default()).unwrap();
-    error!("错误");
 }
